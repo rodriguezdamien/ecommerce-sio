5ecea85225349c79a09fb23c3f888fcf54290bc5
neovenise <rodriguezdamien1677@gmail.com>
2023-11-04 17:56:05 +0100

    update mockup current state

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./mockup/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./mockup/index.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-news":
+                    "radial-gradient(circle, rgba(246,141,72,1) 0%, rgba(237,30,99,1) 53%, rgba(55,37,105,1) 100%)",
+            },
+        },
+        fontFamily: {
+            sans: ["banana-grotesk", "sans-serif"],
+        },
+    },
+    plugins: [],
+};
