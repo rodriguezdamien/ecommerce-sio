8e9968850891561d78411c2ee634c28b6bf8a5e1
neovenise <rodriguezdamien1677@gmail.com>
2023-11-21 19:10:09 +0100

    Mockup Update #1 - 21/11/2023

--- mockup/js/accueil.js
+++ mockup/js/accueil.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var carouselTrending = new Splide(".trending");
+    var carouselTrending = new Splide(".splide",{
+        perPage: 5,
+        rewind:true,
+        width:'1280px',
+        gap:'10px',
+        breakpoints:{
+            1280:{
+                perPage:4,
+                width:'900px'
+            }
+            
+        },
+    });
+    carouselTrending.mount();
 });
