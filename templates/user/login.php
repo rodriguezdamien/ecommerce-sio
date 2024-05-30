0762e6e90b418d8813181a65c7242b31dc9d7fd4
Damien R. <damien@damienrodriguez.fr>
2024-05-31 00:28:14 +0200

    add success message for password change confirmation in login

--- templates/user/login.php
+++ templates/user/login.php
@@ -1,13 +1,18 @@
 <section id="looking-for" class="bg-gradient-sunset flex-1">
-            <div class="flex justify-center items-center min-h-[500px] h-[77vh]">
-                <div class="sm:m-auto bg-white text-black font-extralight px-10 py-4 my-10 rounded-lg">
-                    <h2 class="text-3xl font-extrabold mt-5 mb-7 text-center">Connexion</h2>
+            <div class="flex flex-col justify-center items-center min-h-[500px] h-[77vh]">
+           
+                <div class="sm:mx-auto bg-white text-black font-extralight px-10 py-4 rounded-lg relative">
+                
+                    <h2 class="text-3xl font-extrabold mt-5 mb-4 text-center">Connexion</h2>
                     <?php if (isset($params['error']) && !empty($params['error'])): ?>
-                        <div class="mb-5 border-2 border-red-500 bg-red-200 text-red-700 rounded-lg p-3"><?= $params['error'] ?></div>
+                        <div class="mb-5 border-2 border-red-500 bg-red-200 text-red-700 rounded-lg p-3 text-center"><p><?= $params['error'] ?></p></div>
+                    <?php endif; ?>
+                    <?php if (isset($params['success']) && !empty($params['success'])): ?>
+                        <div class="mb-5 border-2 border-green-500 bg-green-200 text-green-700 rounded-lg p-3 max-w-[550px] text-center"><p><?= $params['success'] ?></p></div>
                     <?php endif; ?>
                     <form action="/login" method="post" class="flex flex-col text-left gap-5 items-center">
                         <div>
-                            <label for="mail"><p class="mb-1">Adresse mail</p></label>
+                            <label for="mail"><p class=" mt-1 mb-1">Adresse mail</p></label>
                             <input
                                 name="mail"
                                 id="mail"
