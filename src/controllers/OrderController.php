8683bce57a58306cde7bc24a38d0d9e48a5c8299
Damien R. <damien@damienrodriguez.fr>
2024-05-31 00:39:33 +0200

    add the ability for admin to see any orders

--- src/controllers/OrderController.php
+++ src/controllers/OrderController.php
@@ -2,6 +2,7 @@
 require_once ('Controller.php');
 require_once ('src/models/AlbumManager.php');
 require_once ('src/models/OrderManager.php');
+require_once ('src/models/UserManager.php');
 
 class OrderController extends Controller
 {
@@ -11,8 +12,8 @@ class OrderController extends Controller
             $params['view'] = '';
             $scripts = [];
             $params['order'] = OrderManager::getOrder($params['idOrder']);
-            if ($params['order']->GetIdUser() != $_SESSION['id']) {
-                header('Location: /');
+            if ((!isset($_SESSION['id']) || $params['order']->GetIdUser() != $_SESSION['id']) && !UserManager::CheckAdmin($_SESSION['id'])) {
+                header('Location: /account/orders');
                 exit();
             } else {
                 $orderItems = $params['order']->GetOrderItems();
